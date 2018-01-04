6cdc651e5d9cb10c7a7d38b05399676b52f87d7f
julie hawkins <julabi@gmail.com>
2018-01-04 10:39:10 -0700

    creates title display in card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,11 +12,16 @@ const Card = ({ houseData }) => {
     coatOfArms 
   } = houseData;
 
+  const titleDisplay = titles.map((title, index) => {
+    return <p key={`title-${index}`}>title: {title}</p>
+  })
+
   return (
     <div className='Card'>
       <h1>{name}</h1>
       <h2>{words}</h2>
       <h3>{founded}</h3>
+      {titleDisplay}
       <p>{coatOfArms}</p>
     </div>
   )
