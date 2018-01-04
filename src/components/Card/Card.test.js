96b1a078bb9425bd2dc2fa52ec4f1ebfd795cfa7
julie hawkins <julabi@gmail.com>
2018-01-04 15:13:19 -0700

    adds new card snapshot with store

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
 describe('Card test', () => {
-  it('should match its snap shot', () => {
-    const mockHouseData = {
-      name: 'House Hightower of the Hightower', 
-      words: 'We Light the Way', 
-      founded: 'Age of Dawn', 
-      seats: ['The Hightower, Oldtown'], 
-      titles: [
-        'Lord of the Hightower',
-        'Lord of the Port',
-        'Voice of Oldtown',
-        'Defender of the Citadel',
-        'Beacon of the South',
-        'King of the High Tower (formerly)'
-      ], 
-      ancestralWeapons: ['Vigilance'], 
-      coatOfArms: 'Cendrée, a tower argent with a beacon on fire gules'  
+  const fakeStore = configureMockStore()({
+    currentSwornData: []
+  });
+
+
+  const setup = () => {
+    const props = {
+      showMoreDetails: jest.fn()
     };
 
-    const renderedCard = shallow(<Card houseData={mockHouseData}/>);
+    const mockHouseDataProps = {
+        name: 'House Hightower of the Hightower', 
+        words: 'We Light the Way', 
+        founded: 'Age of Dawn', 
+        seats: ['The Hightower, Oldtown'], 
+        titles: [
+          'Lord of the Hightower',
+          'Lord of the Port',
+          'Voice of Oldtown',
+          'Defender of the Citadel',
+          'Beacon of the South',
+          'King of the High Tower (formerly)'
+        ], 
+        ancestralWeapons: ['Vigilance'], 
+        coatOfArms: 'Cendrée, a tower argent with a beacon on fire gules'  
+      };
 
-    expect(renderedCard).toMatchSnapshot();
+    const wrapper = shallow(
+      <Provider store={fakeStore}>
+        <Card houseData={mockHouseDataProps}/>
+      </Provider>
+    );
+    const Component = wrapper.find(Card);
+
+    return {
+      Component
+    };
+  };
+
+  it('should match its snap shot', () => {
+    
+    expect(setup.wrapper).toMatchSnapshot();
   });
 });
